3482165d357beb808b2248e8507fb00e1750b034
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-02 13:43:07 -0300

    Create quest_15.py

---
+++ Estrutura Sequencial/quest_15.py
@@ -0,0 +1,13 @@
+def main():
+  print('Calcular a Área de um Triângulo')
+  print('-------')
+  
+  base = float(input('Qual o valor da base?: '))
+  altura = float(input('Qual o valor da altura?: '))
+  
+  area = ( base * altura / 2 )
+  
+  print('-----')
+  print(f'A área desse triângulo é {area}!')
+
+main()
