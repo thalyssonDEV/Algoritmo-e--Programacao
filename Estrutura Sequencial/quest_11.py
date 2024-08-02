be03ce66854a790bb6ed2f49ac4bdd50b76de032
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-02 13:30:30 -0300

    Create quest_11.py

---
+++ Estrutura Sequencial/quest_11.py
@@ -0,0 +1,14 @@
+def main():
+  print('Inventer um número inteiro de 3 dígitos')
+  print('---------')
+  
+  num = int(input('Digite o Número: '))
+  print('---------')
+  
+  alg1 = num // 100
+  alg2 = num % 100 // 10
+  alg3 = num % 10
+  
+  print(f'A inversão do seu número é {alg3}{alg2}{alg1}!')
+
+main()
