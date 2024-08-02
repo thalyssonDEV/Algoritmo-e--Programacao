c75c71c7ffcaf193eeb12bc90e239ec4381b4c89
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-02 13:17:30 -0300

    Create quest_7.py

---
+++ Estrutura Sequencial/quest_7.py
@@ -0,0 +1,16 @@
+def main():
+  print ('Operações com Inteiros |A,B,C|')
+  print ('------------')
+  
+  a = int(input('Número A: '))
+  b = int(input('Número B: '))
+  c = int(input('Número C: '))
+  print('--------------')
+  
+  soma_ab = a + b
+  dif_bc = b - c
+  
+  print(f'A soma entre A e B = [{soma_ab}]')
+  print(f'A diferença entre B e C = [{dif_bc}]')
+  
+main()
