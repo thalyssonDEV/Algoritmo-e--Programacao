518a369ec47856691c9e78494107e5f3b8993b86
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-02 12:49:25 -0300

    Update quest_1.py

--- Estrutura Sequencial/quest_1.py
+++ Estrutura Sequencial/quest_1.py
@@ -1,1 +1,11 @@
+def main():
+  print (' Calculadora de m/s para km/h')
+  print ('----------------')
+  
+  ms = int(input('Qual valor deseja converter? '))
+  
+  kh = ms * 3.6
+  
+  print (f' {ms} M/s são {kh} Km/h ')
 
+main()
