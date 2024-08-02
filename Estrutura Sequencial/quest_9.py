39caa18ee0c75f4afe3ecc889438c4bc94b7d7c8
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-02 13:22:15 -0300

    Create quest_9.py

---
+++ Estrutura Sequencial/quest_9.py
@@ -0,0 +1,13 @@
+def main():
+  print('Inverter Números ')
+  print('----------')
+  
+  a = int(input('Digite o 1° Número: '))
+  b = int(input('Digite o 2° Número: '))
+  
+  print('-----------')
+  print(f'Seus Números são ({a},{b})')
+  print('---------')
+  print(f'A inversão deles é ({b},{a})')
+
+main()
