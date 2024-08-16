12a4c20afa2d976fffac180c31b8cbb2a6694607
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-15 22:40:45 -0300

    Update menu_string.py

--- menu_string.py
+++ menu_string.py
@@ -3,113 +3,148 @@ def clean_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def remove_space(word):
+   
     new_word = ''
     for character in word:
-
         if character != ' ':
             new_word += character
-        else:
-            continue
-
     return new_word
 
-def option_1(fin,quantity_letters):
-    for line in fin:
-
-        word = line.strip()
-        formated_word = remove_space(word)
-        if len(formated_word) > quantity_letters:
-            print(formated_word)
-        else:
-            continue
-
-def option_2(fin,letter_to_remove):         
-    total_words = 0
-    deleted_words = 0
-    for line in fin:
-        total_words += 1
-
-        word = line.strip()
-        formated_word = remove_space(word)
-
-        new_word = ''
-        for character in formated_word:
-
-            if character != letter_to_remove:
-                new_word += character
-        deleted_words += 1
-        
-        if new_word != '':
-            print(new_word)
+def option_1(fin, quantity_letters):
     
-    print(total_words)
-    print(deleted_words)
-
-def option_3(fin,removed_words):
-    list_removed_words = []
-    for index in removed_words:
+    results = []
+    for line in fin:
+        word = line.strip()
+        formatted_word = remove_space(word)
         
-        if index != " ":
-            list_removed_words.append(index)
+        if len(formatted_word) > quantity_letters:
+            results.append(formatted_word)
+    return results
 
+def option_2(fin, letter_to_remove):
+    
+    results = []
+    total_words = 0
+    count_without_letter = 0
+
+    for line in fin:
+        words = line.strip().split()
+        total_words += len(words)
+
+        for word in words:
+            formatted_word = remove_space(word)
+            
+            if letter_to_remove not in formatted_word:
+                results.append(formatted_word)
+                count_without_letter += 1
+
+    if total_words > 0:
+        percentage = (count_without_letter / total_words) * 100
+    else:
+        percentage = 0
+
+    return results, percentage
+
+def option_3(fin, removed_words):
+    
+    list_removed_words = [index for index in removed_words if index != " "]
     words_without_prohibited_letters = 0
     for line in fin:
-        for letter in line:
-            if letter in list_removed_words:
-                break
-            else:
-                words_without_prohibited_letters += 1
-    
-    print(f"\033[35mO Arquivo Possui {words_without_prohibited_letters} Palavras que Não Incluem esses Caracteres\033[m")
-
-def option_4(word,characters_in_word):
-    list_letters = []
-    for index in characters_in_word:
-
-        if index != " ":
-            list_letters.append(index)
-
-    for letter in word:
-        if letter in list_letters:
-            print("\033[95mA Palavra Contém Pelo Menos 1 Caractere da Lista\033[m")
-            break
-    else:
-        print("\033[95mA Palavra Não Contém Nenhum Caractere da Lista\033[m")
-    
-# def option_5(fin,mandatory_letters):
-#     list_letters = []
-#     for index in mandatory_letters:
+        words = line.strip().split()
         
-#         if index != " ":
-#             list_letters.append(index)
+        for word in words:
+            
+            if all(letter not in list_removed_words for letter in word):
+                words_without_prohibited_letters += 1
+    return words_without_prohibited_letters
 
-#     for lines in fin:
-#         for letters in lines:
-#             if letters not in list_letters:
-#                 break
-#         print(lines)
+def option_4(fin, characters_in_word):
+    
+    list_letters = [index for index in characters_in_word if index != " "]
+
+    count_words_with_letters = 0
+    for line in fin:
+        words = line.strip().split()
+
+        for word in words:
+            formatted_word = remove_space(word)
+            contains_letter = False
+
+            for letter in list_letters:
+                
+                if letter in formatted_word:
+                    contains_letter = True
+                    break
+            if contains_letter:
+                count_words_with_letters += 1
+
+    return count_words_with_letters
+
+def option_5(fin, required_letters):
+    list_required_letters = [index for index in required_letters if index != " "]
+
+    count_words_with_all_letters = 0
+    for line in fin:
+        words = line.strip().split()
+
+        for word in words:
+            formatted_word = remove_space(word)
+            has_all_letters = True
+
+            for letter in list_required_letters:
+                
+                if letter not in formatted_word:
+                    has_all_letters = False
+                    break
+            if has_all_letters:
+                count_words_with_all_letters += 1
+
+    return count_words_with_all_letters
+
+def option_6(fin):
+    results = []
+    count_words_alphabetical = 0
+
+    for line in fin:
+        words = line.strip().split()
+
+        for word in words:
+            formatted_word = remove_space(word)
+            is_alphabetical = True
+
+            for i in range(len(formatted_word) - 1):
+                if formatted_word[i] > formatted_word[i + 1]:
+                    is_alphabetical = False
+                    break
+
+            if is_alphabetical:
+                results.append(formatted_word)
+                count_words_alphabetical += 1
+
+    return results, count_words_alphabetical
 
 def show_menu():
     print("""\033[36m
-        ======== WORD-PLAY ========\033[m
+      ======== WORD-PLAY ========\033[m
 \033[33m[ 1 ] - PALAVRAS COM MAIS DE 20 LETRAS
 [ 2 ] - PALAVRAS SEM A LETRA "E"
 [ 3 ] - PALAVRAS SEM "X" LETRAS
-[ 4 ] - EM MANUTENÇÃO
-[ 5 ] - 
+[ 4 ] - PALAVRAS COM LETRAS ESPECÍFICAS
+[ 5 ] - PALAVRAS COM LETRAS OBRIGATÓRIAS
+[ 6 ] - PALAVRAS COM LETRAS EM ORDEM ALFABÉTICA
 [ 0 ] - SAIR
 \033[m""")
 
 def main():
     show_menu()
-    
-    fin = open(str(input("""Informe o Arquivo [Ex: \033[31marquivo.txt\033[m]:
---> """)), "r")
+
+    filename = input("""Informe o Arquivo [Ex: \033[31marquivo.txt\033[m]:
+--> """)
     clean_screen()
-    print(f"ARQUIVO \033[33m{fin.name}\033[m CARREGADO COM SUCESSO")
+    print(f"ARQUIVO \033[33m{filename}\033[m CARREGADO COM SUCESSO")
     input("\033[34m\nPressione ENTER para continuar\033[m")
     clean_screen()
-    
+
     choice = 1
     while choice != 0:
         show_menu()
@@ -117,28 +152,50 @@ def main():
         clean_screen()
 
         if choice == 1:
-            option_1(fin,20)
+            fin = open(filename, "r")
+            results = option_1(fin, 20)
+            for result in results:
+                print(result)
+            fin.close()
 
-        if choice == 2:
-            option_2(fin,"e")
+        elif choice == 2:
+            fin = open(filename, "r")
+            results, percentage = option_2(fin, "e")
+            for result in results:
+                print(result)
+            print(f'\n\033[35mA Porcentagem de Palavras Que Não Contêm a Letra "E" é {percentage:.2f}%\033[m')
+            fin.close()
 
-        if choice == 3:
-            removed_words = str(input("""Digite as Letras que Deseja Remover Separadas por Espaço [\033[31mEx: a b c d\033[m]:
---> """))
-            option_3(fin,removed_words)
-        
-        if choice == 4:
-            word = str(input("Digite a Palavra: "))
-            characters_in_word = str(input("""Digite as Letras que Devem estar Contidas na Palavra [\033[31mEx: a b c d\033[m]:
---> """))
+        elif choice == 3:
+            removed_words = input("""Digite as Letras que Deseja Remover Separadas por Espaço [\033[31mEx: a b c d\033[m]:
+--> """)
+            fin = open(filename, "r")
+            words_without_prohibited_letters = option_3(fin, removed_words)
+            print(f"\033[35mO Arquivo Possui {words_without_prohibited_letters} Palavras que Não Incluem esses Caracteres\033[m")
+            fin.close()
 
-            option_4(word,characters_in_word)
+        elif choice == 4:
+            fin = open(filename, "r")
+            characters_in_word = input("""Digite as Letras que Devem Estar Contidas nas Palavras [\033[31mEx: a b c d\033[m]:
+--> """)
+            count_words_with_letters = option_4(fin, characters_in_word)
+            print(f"\033[95mO Arquivo Possui {count_words_with_letters} Palavras que Contêm Pelo Menos 1 Caractere da Lista\033[m")
+            fin.close()
 
-        fin = open(fin.name)
+        elif choice == 5:
+            fin = open(filename, "r")
+            required_letters = input("""Digite as Letras que Devem Estar Presentes em Todas as Palavras [\033[31mEx: a b c d\033[m]:
+--> """)
+            count_words_with_all_letters = option_5(fin, required_letters)
+            print(f"\033[92mO Arquivo Possui {count_words_with_all_letters} Palavras que Contêm Todas as Letras Obrigatórias\033[m")
+            fin.close()
 
-#         if choice == 5:
-#             mandatory_letters = str(input("""Digite as Letras Obrigatórias nas Palavras [\033[31mEx: a b c d\033[m]:
-# --> """))
-#             option_5(fin,mandatory_letters)
+        elif choice == 6:
+            fin = open(filename, "r")
+            results, count_words_alphabetical = option_6(fin)
+            for result in results:
+                print(result)
+            print(f"\n\033[96mO Arquivo Possui {count_words_alphabetical} Palavras cujas Letras Estão em Ordem Alfabética\033[m")
+            fin.close()
 
 main()
