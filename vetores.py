7f8c4771bd158a71c32dcde9945444bd0a582f65
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-08-23 14:07:43 -0300

    Create vetores.py

---
+++ vetores.py
@@ -0,0 +1,42 @@
+import utils as utils
+
+def menu():
+    print("""        \033[35m====== MANIPULADOR DE VETORES =======\033[m
+[ 1 ] - INICIALIZAR VETOR
+[ 2 ] - MOSTRAR TODOS OS VALORES DO VETOR
+          
+          """)
+
+
+
+
+
+
+def main():
+    name = str(input(utils.color("Digite Seu Nome de Usuário: ",36)))
+    utils.clean_screen()
+
+    print(utils.color((f"\nOlá {name}, Bem Vindo ao Manipulador de Vetores, Verifique o Menu e Selecione sua Opção\n"),34))
+    menu()
+
+    choice = 1
+    while choice != 0:
+        choice = int(input(utils.color("\nDigite sua Escolha: ", 36)))
+
+        if choice == 1:
+            tamanho,maximo,minimo = map(int,input(utils.color("""\nDigite o Tamanho, Número Mínimo e Número Máximo do seu Vetor, Respectivamente [Ex: 10 1 100]:
+--> """,36)).split())
+            vetor_atual = utils.get_random_vector(tamanho,maximo,minimo)
+            utils.clean_screen()
+            print(utils.color("VETOR ATUAL -->",34),vetor_atual,"\n")
+            utils.wait_press_enter()
+            utils.clean_screen()
+            menu()
+        
+        if choice == 2:
+            print(utils.color("VETOR ATUAL -->",34),vetor_atual,"\n")
+            utils.wait_press_enter()
+            utils.clean_screen()
+            menu()
+
+main()
